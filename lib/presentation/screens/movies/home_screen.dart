80f1d4ee440b2d76e0245ea4941735108c033975
RiosHectorM <rios.hector.martin@gmail.com>
2024-09-18 22:43:39 -0300

    GetData TheMovieDB

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
@@ -7,9 +6,9 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
-        child: Text( dotenv.env['THE_MOVIEDB_KEY'] ?? 'SIN API KEY'),
+        child: Placeholder(),
       ),
     );
   }
