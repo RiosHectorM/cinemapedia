7f3d8f9111a79f75e03d7c07f0cae1129d5c4efd
RiosHectorM <rios.hector.martin@gmail.com>
2024-09-20 23:32:51 -0300

    SlideShow ok

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -34,20 +34,13 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   @override
   Widget build(BuildContext context) {
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    final moviesSlideShow = ref.watch(moviesSlideshowProvider);
+
     if (nowPlayingMovies.isEmpty) return const CircularProgressIndicator();
     return Column(
       children: [
         const CustomAppbar(),
-        MoviesSlideShow(movies: nowPlayingMovies)
-        // Expanded(
-        //     child: ListView.builder(
-        //         itemCount: nowPlayingMovies.length,
-        //         itemBuilder: (context, index) {
-        //           final movie = nowPlayingMovies[index];
-        //           return ListTile(
-        //             title: Text(movie.title),
-        //           );
-        //         }))
+        MoviesSlideShow(movies: moviesSlideShow)
       ],
     );
   }
