728e5abe1c60b4b960030fb734a72a20ef29c5ec
RiosHectorM <rios.hector.martin@gmail.com>
2024-09-28 23:12:03 -0300

    GetDetails Implementation

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,6 +1,9 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/movies/movie_info_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class MovieScreen extends StatelessWidget {
+class MovieScreen extends ConsumerStatefulWidget {
   static const name = 'movie-screen';
 
   final String movieID;
@@ -8,9 +11,32 @@ class MovieScreen extends StatelessWidget {
   const MovieScreen({super.key, required this.movieID});
 
   @override
+  MovieScreenState createState() => MovieScreenState();
+}
+
+class MovieScreenState extends ConsumerState<MovieScreen> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(movieInfoProvider.notifier).loadMovie(widget.movieID);
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final Movie? movie = ref.watch(movieInfoProvider)[widget.movieID];
+
+    if (movie == null) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(
+            strokeWidth: 2,
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
-      appBar: AppBar(title: Text(movieID)),
+      appBar: AppBar(title: Text(widget.movieID)),
     );
   }
 }
